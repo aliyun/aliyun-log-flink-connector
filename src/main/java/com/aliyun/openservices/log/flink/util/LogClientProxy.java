5901582c2730bdc0a57ab8ccea41868f700f5800
liketic <ke.like@outlook.com>
2022-01-24 15:47:08 +0800

    refine imports

--- src/main/java/com/aliyun/openservices/log/flink/util/LogClientProxy.java
+++ src/main/java/com/aliyun/openservices/log/flink/util/LogClientProxy.java
@@ -22,7 +22,6 @@ import org.slf4j.LoggerFactory;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.regex.Pattern;
 
